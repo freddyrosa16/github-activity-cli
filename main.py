320b1868fcb4fc0f881a3e9cac1ed6c10e9237ec
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-27 22:14:08 -0400

    refractoring for PushEvent

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import requests
 
 
 def main():
+    print("Output:")
     url = "https://api.github.com/users/freddyrosa16/events"
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        for event in data:
+            event_type = event["type"]
+            repo_name = event["repo"]["name"]
+
+            if event == "PushEvent":
+                commit_count = event["payload"].get("size", 0)
+                print(f"Pushed {commit_count} commits to {repo_name}")
     else:
         print(f"Error: {response.status_code}")
 
